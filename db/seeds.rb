06d6dc90f07ac12c1e6a21cd4d00fa5d19c08301
Kelvin Lee <kelvin_lee@leaderradio.cn>
2019-03-26 17:07:58 +0800

    second challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(title: Faker::Quote.famous_last_words, content: Faker::Lorem.paragraph)
+end
+
+# Faker::Quote.famous_last_words
+# Faker::Lorem.paragraphs
